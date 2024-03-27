9f361ec4fff8a76cc0b3680e0a62a69942b67fda
johannes <johannes.muljana@ait.nsw.edu.au>
2024-03-27 15:58:59 +1100

    Added Theme

--- components/AuthenticationForm.js
+++ components/AuthenticationForm.js
@@ -1,12 +1,13 @@
 import { View, Text, TextInput, StyleSheet, Pressable } from "react-native"
+import { Theme } from "../components/Theme"
 
 export function AuthenticationForm ( props ) {
     return (
         <View style={ styles.form }>
             <Text style={ styles.title }>{ props.title }</Text>
-            <Text>Email</Text>
+            <Text style={ styles.label }>Email</Text>
             <TextInput style={ styles.input } />
-            <Text>Password</Text>
+            <Text style={ styles.label }>Password</Text>
             <TextInput style={ styles.input } secureTextEntry={true} />
             <Pressable style={ styles.button }>
                 <Text style={ styles.buttonText }>{ props.action }</Text>
@@ -25,15 +26,20 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20
     },
+    label: {
+        marginTop: 15,
+    },
     input: {
         borderBottomColor: "#666666",
-        borderBottomWidth: 2,
+        borderBottomWidth: 1,
         padding: 5,
+        marginTop: 2,
     },
     button: {
         marginTop: 30,
-        backgroundColor: "darkblue",
-        padding: 10,
+        backgroundColor: Theme.secondary,
+        padding: 12,
+        borderRadius: 10,
     },
     buttonText: {
         color: "white",
---
+++ components/Theme.js
@@ -0,0 +1,7 @@
+export const Theme  = {
+   primary: "hsla(213, 74%, 15%, 1)",
+   primaryMid: "hsla(213, 74%, 65%, 1)",
+   primaryLight: "hsla(213, 74%, 85%, 1)",
+   secondary:"hsla(338, 87%, 67%, 1)",
+   tertiary: "hsla(168, 58%, 41%, 1)",
+}
