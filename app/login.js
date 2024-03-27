ec568b5dcce1ad5c136369b0e4c64fe873585a56
johannes <johannes.muljana@ait.nsw.edu.au>
2024-03-27 15:58:26 +1100

    added login

---
+++ app/login.js
@@ -0,0 +1,21 @@
+import { View, Text, SafeAreaView, StyleSheet} from "react-native"
+import { Link } from "expo-router"
+import { AuthenticationForm } from "../components/AuthenticationForm"
+import { AlternateAuthBtn } from "../components/AlternateAuthBtn"
+
+export default function Login () {
+    return (
+        <SafeAreaView style={styles.container}>
+            <AuthenticationForm title="Login to your account" action="Sign in"/>
+            <AlternateAuthBtn text="Don't have an account?" link="/" linkText="sign up here"/>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "lightblue",
+        justifyContent: "center",
+    },
+})
