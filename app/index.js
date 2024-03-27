94aed8e7bf3dfd445c0b75ca0af9f6a4a3b0b8f8
johannes <johannes.muljana@ait.nsw.edu.au>
2024-03-27 15:58:47 +1100

    Added alternate auth

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 import { View, Text, SafeAreaView, StyleSheet} from "react-native"
 import { Link } from "expo-router"
+import { Theme } from "../components/Theme"
 import { AuthenticationForm } from "../components/AuthenticationForm"
+import { AlternateAuthBtn } from "../components/AlternateAuthBtn"
+
 export default function Home () {
     return (
         <SafeAreaView style={styles.container}>
             <AuthenticationForm title="Register for an account" action="Sign up"/>
+            <AlternateAuthBtn text="Already have an account?" link="/login" linkText="sign in here"/>
         </SafeAreaView>
     )
 }
@@ -12,7 +16,7 @@ export default function Home () {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "lightblue",
+        backgroundColor: Theme.primaryLight,
         justifyContent: "center",
     },
 })
---
+++ components/AlternateAuthBtn.js
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+
+export function AlternateAuthBtn( props ) {
+    return (
+        <View style={styles.button}>
+            <Text>{ props.text } <Link href={ props.link }>{ props.linkText }</Link></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        marginHorizontal: 20,
+        marginVertical: 20,
+    },
+    buttonText: {
+        textAlign: "center",
+    }
+})
