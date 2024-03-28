a41b81a012082d1038b05c9403cd5a3d05d589cb
johannes <johannes.muljana@ait.nsw.edu.au>
2024-03-28 19:13:12 +1100

    adding email and password validation

--- components/AuthenticationForm.js
+++ components/AuthenticationForm.js
@@ -1,13 +1,46 @@
 import { View, Text, TextInput, StyleSheet, Pressable } from "react-native"
+import { useState, useEffect } from "react"
 
 export function AuthenticationForm ( props ) {
+    const [ email, setEmail ] = useState('')
+    const [ validEmail, setValidEmail ] = useState(false)
+    const [ password, setPassword ] = useState('')
+    const [ validPassword, setValidPassword ] = useState(false)
+
+    useEffect( () => {
+        if( email.indexOf('@') > 0 && email.indexOf('.') > 0 ) {
+            setValidEmail( true )
+        }
+        else {
+            setValidEmail( false )
+        }
+    }, [ email ])
+
+    useEffect( () => {
+        if( password.length >= 8 ) {
+            setValidPassword( true )
+        }
+        else {
+            setValidPassword( false )
+        }
+    }, [password])
+
     return (
         <View style={ styles.form }>
             <Text style={ styles.title }>{ props.title }</Text>
             <Text>Email</Text>
-            <TextInput style={ styles.input } />
+            <TextInput 
+                style={ (validEmail) ? styles.validInput : styles.input } 
+                value={ email }
+                onChangeText={ (text) => setEmail(text) }
+            />
             <Text>Password</Text>
-            <TextInput style={ styles.input } secureTextEntry={true} />
+            <TextInput 
+                secureTextEntry={true} 
+                value={ password }
+                onChangeText={ (text) => setPassword(text) }
+                style={ (validPassword) ? styles.validInput : styles.input } 
+            />
             <Pressable style={ styles.button }>
                 <Text style={ styles.buttonText }>{ props.action }</Text>
             </Pressable>
@@ -27,6 +60,11 @@ const styles = StyleSheet.create({
     },
     input: {
         borderBottomColor: "#666666",
+        borderBottomWidth: 2,
+        padding: 5,
+    },
+    validInput: {
+        borderBottomColor: "#0ac43c",
         borderBottomWidth: 2,
         padding: 5,
     },
