dd06762d8cbab20fe96ec417b8d131625aca7533
johannes <johannes.muljana@ait.nsw.edu.au>
2024-04-11 19:54:30 +1000

    added flatlist

--- app/home.js
+++ app/home.js
@@ -1,28 +1,77 @@
-import { View, Text, TextInput, Pressable, StyleSheet } from "react-native"
+import { View, Text, TextInput, Pressable, FlatList, StyleSheet } from "react-native"
 import { useState, useEffect, useContext } from 'react'
 import { Theme } from "../theme/Theme"
 import { Stack } from "expo-router"
 import { DBContext } from "../contexts/DBContext"
+import { AuthContext } from "../contexts/AuthContext"
+import { collection, addDoc, query, getDocs } from "firebase/firestore"
 
 export default function Home( props ) {
     const [ started, setStarted ] = useState( false )
+    const [ startTime, setStartTime ] = useState()
+    const [ stopTime, setStopTime ] = useState()
+    const [ taskName, setTaskName] = useState()
+    const [ listData, setListData ] = useState([])
 
+    const auth = useContext( AuthContext )
+    const userId = auth.currentUser.uid
     const db = useContext( DBContext )
 
-    const manageTask = () => {
-
+    const getTime = () => {
+        return new Date().getTime()
     }
+
+    const manageTask = () => {
+        if( !started ) {
+            setStarted( true )
+            setStartTime( getTime() )
+        }
+        else {
+            setStarted( false )
+            setStopTime( getTime() )
+        }
+    }
+
+    const saveTask = async () => {
+        const task = { start: startTime, stop: stopTime, name: taskName }
+        const dbcollection = collection( db, `usertasks/${userId}/tasks`)
+        const docRef = await addDoc( dbcollection, task )
+    }
+
+    const readData = async () => {
+        const q = query( collection( db, `usertasks/${userId}/tasks` ) )
+        const querySnapshot = await getDocs( q )
+        let data = []
+        querySnapshot.forEach( (doc) => {
+            let item = doc.data()
+            item.id = doc.id
+            data.push( item )
+        })
+        setListData( data )
+    }
+
+    useEffect( () => {
+        readData()
+    }, [started])
 
     return (
         <View style={ styles.container }>
             <Stack.Screen options={{ headerShown: true }}/>
             <View style={ styles.form }>
                 <Text>Task Name</Text>
-                <TextInput />
-                <Pressable style={ styles.button }>
-                    <Text style={ styles.buttonText }>Start</Text>
+                <TextInput value={taskName} onChangeText={ (text) => setTaskName(text) } />
+                <Pressable style={ styles.button } onPress={ () => manageTask() }>
+                    <Text style={ styles.buttonText }>{ (started) ? "Stop" : "Start"}</Text>
+                </Pressable>
+                <Pressable onPress={ () => saveTask() } >
+                    <Text>Save Task</Text>
                 </Pressable>
             </View>
+            <FlatList 
+                data={ listData }
+                renderItem={ ({item}) => ( <Text>{ item.name }</Text>) }
+                keyExtractor={(item) => item.id }
+            />
         </View>
     )
 }
